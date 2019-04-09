bf010e2b2f0491fdc5c7104169bd935b3bd2e87e
Laspargus <maxime.speroni@gmail.com>
2019-04-09 14:36:40 +0200

    suppress empty view test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,6 +64,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "localhost",
-    :port => 1030
+    :port => 1025,
   }  
+
+
 end
